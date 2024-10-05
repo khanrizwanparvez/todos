d1d5ce5d2245e9f95d47caad7f833c26df9c0234
khanrizwanparvez <khanrizwanparvez@gmail.com>
2024-10-05 14:14:57 +0600

    Create route & POST method for add task

--- app/api/new-task/route.js
+++ app/api/new-task/route.js
@@ -1,11 +1,27 @@
-export const POST = (req) => {
+import { catchAsyncErrors } from "@/middlewares/error";
+import { Task } from "@/models/task";
+import { connectDB } from "@/utils/features";
 
-    
+export const POST = catchAsyncErrors(async (req) => {
+  await connectDB();
+
+  const body = await req.json();
+
+  const { title, description } = body;
+
+  await Task.create({
+    title,
+    description,
+    user: "60f7a1e4e3c543b288cd0a9a",
+  });
+
   return new Response(
-    {
+    JSON.stringify({
       success: true,
-      message: "Route working",
-    },
-    { status: 200 }
+      message: "Task added successfully",
+    }),
+    {
+      status: 201,
+    }
   );
-};
+});
